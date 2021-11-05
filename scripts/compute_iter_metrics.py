fe233d31276b346822ced878cb0ab980d367fba5
Max Gardner <gardner.max@gmail.com>
2021-11-04 17:47:36 -0700

    works but 2e6 alts too slow

--- scripts/compute_iter_metrics.py
+++ scripts/compute_iter_metrics.py
@@ -8,10 +8,80 @@ import argparse
 import large_mnl as lmnl
 
 
+def create_data(num_alts, num_choosers, num_vars=5):
+
+    # create choosers
+    choosers = np.random.lognormal(0, 0.5, num_choosers)  # 1 chooser attribute
+
+    # initialize alts
+    alts = np.zeros((num_alts, num_vars))
+
+    # dist to CBD
+    alts[:, 0] = np.random.lognormal(0, 1, num_alts)  # Dist to CBD
+
+    # size terms
+    for i in range(1, num_vars - 1):
+        split_val = int(np.floor(num_alts * .5))
+        alts[:split_val, i] = np.random.normal(
+            1, 1, split_val)  # first 1/2 of alts have mean = 1
+        alts[split_val:, i] = np.random.normal(
+            0.5, 1, num_alts - split_val)  # rest of alts have mean 0.5
+
+    # interaction term
+    alts[:, -1] = np.random.lognormal(1, .5, num_alts)
+
+    return choosers, alts
+
+
+def process_iter_prob_metrics(num_alts, result_metrics, sample_rates):
+
+    iter_metrics = pd.DataFrame()
+    now = time.time()
+    for i, sample_rate in enumerate(sample_rates):
+        sample_metrics = result_metrics[:, i, :]
+        tmsm_mean_err = np.nanmean(sample_metrics[:, 2])
+        tmsm_mean_abs_pct_err = np.nanmean(np.abs(sample_metrics[:, 3]))
+        tmsm_rmse = np.sqrt(np.nanmean(sample_metrics[:, 4]))
+        tmsmc_mean_err = np.nanmean(sample_metrics[:, 5])
+        tmsmc_mean_abs_pct_err = np.nanmean(np.abs(sample_metrics[:, 6]))
+        tmsmc_rmse = np.sqrt(np.nanmean(sample_metrics[:, 7]))
+        cf_mean_err = np.nanmean(sample_metrics[:, 8])
+        cf_mean_abs_pct_err = np.nanmean(np.abs(sample_metrics[:, 9]))
+        cf_rmse = np.sqrt(np.nanmean(sample_metrics[:, 10]))
+        ppgm_mean_err = np.nanmean(sample_metrics[:, 11])
+        ppgm_mean_abs_pct_err = np.nanmean(np.abs(sample_metrics[:, 12]))
+        ppgm_rmse = np.sqrt(np.nanmean(sample_metrics[:, 13]))
+
+        iter_metrics = pd.concat((iter_metrics, pd.DataFrame([{
+            'num_alts': num_alts,
+            'num_choosers': num_choosers,
+            'sample_rate': sample_rate,
+            'tmsm_mean_err': tmsm_mean_err,
+            'tmsm_mean_abs_pct_err': tmsm_mean_abs_pct_err,
+            'tmsm_rmse': tmsm_rmse,
+            'tmsmc_mean_err': tmsmc_mean_err,
+            'tmsmc_mean_abs_pct_err': tmsmc_mean_abs_pct_err,
+            'tmsmc_rmse': tmsmc_rmse,
+            'cf_mean_err': cf_mean_err,
+            'cf_mean_abs_pct_err': cf_mean_abs_pct_err,
+            'cf_rmse': cf_rmse,
+            'ppgm_mean_err': ppgm_mean_err,
+            'ppgm_mean_abs_pct_err': ppgm_mean_abs_pct_err,
+            'ppgm_rmse': ppgm_rmse}])))
+
+    later = time.time()
+    print(
+        "Took {0} s to compute sample metrics for all "
+        "sample rates.".format(np.round(later - now, 1)))
+
+    return iter_metrics
+
+
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument("-b", "--batched", action="store_true")
+    parser.add_argument("-n", "--num_bootstraps", action="store")
     args = parser.parse_args()
     batched = args.batched
 
@@ -22,61 +92,25 @@ if __name__ == '__main__':
     sample_rates = [.1, .2, .3, .4, .5, .6, .7, .8, .9]
     coeffs = np.array([-1, 1, 1, 1, 1])
 
-    metrics = pd.DataFrame(
-        columns=[
-            'num_alts', 'num_choosers', 'sample_rate',
-            'med_pct_probs_gt_pop_mean', 'med_max_corr_prob'])
+    metrics = pd.DataFrame()
 
     key = random.PRNGKey(0)
     for num_alts in alts_sizes:
-        now = time.time()
-        num_alts = int(num_alts)
-        pop_mean_prob = 1 / num_alts
 
-        choosers, alts = lmnl.create_data(num_alts, pop_to_alts_ratio)
+        # get data
+        num_alts = int(num_alts)
+        num_choosers = int(pop_to_alts_ratio * num_alts)
+        pop_mean_prob = 1 / num_alts
+        choosers, alts = create_data(num_alts, num_choosers)
         num_choosers = choosers.shape[0]
         print(num_alts, " ALTS, ", num_choosers, " CHOOSERS")
-        probs_true_max, pct_true_probs_gt_pop_mean = lmnl.get_probs(
+
+        # compute probs and metrics
+        result_metrics = lmnl.get_iter_probs(
             choosers, alts, key, num_alts, batched)
-        later = time.time()
-        print("Took {0} s to compute true probs.".format(
-            np.round(later - now)))
+        num_alt_metrics = process_iter_prob_metrics(
+            num_alts, result_metrics, sample_rates)
 
-        now = time.time()
-        med_pct_probs_gt_pop_mean = np.median(pct_true_probs_gt_pop_mean)
-        med_max_prob = np.median(probs_true_max)
-        later = time.time()
-        print("Took {0} s to compute true prob. metrics".format(
-            np.round(later - now)))
-
-        metrics = pd.concat((metrics, pd.DataFrame([{
-            'num_alts': num_alts,
-            'num_choosers': num_choosers,
-            'sample_rate': 1,
-            'med_pct_probs_gt_pop_mean': med_pct_probs_gt_pop_mean,
-            'med_max_corr_prob': med_max_prob}])))
-
-        for sample_rate in sample_rates:
-            now = time.time()
-
-            sample_size = int(num_alts * sample_rate)
-            probs_samp_max, pct_samp_probs_gt_pop_mean = lmnl.get_probs(
-                choosers, alts, key, sample_size, batched)
-            later = time.time()
-            print("Took {0} s to compute {1}% sample probs.".format(
-                np.round(later - now), sample_rate * 100))
-
-            # metrics
-            now = time.time()
-            med_pct_probs_gt_pop_mean = np.median(pct_samp_probs_gt_pop_mean)
-            med_max_corr_prob = np.median(probs_samp_max) * sample_rate
-            later = time.time()
-            print("Took {0} s to compute {1}% sample metrics.".format(
-                np.round(later - now), sample_rate * 100))
-
-            metrics = pd.concat((metrics, pd.DataFrame([{
-                'num_alts': num_alts,
-                'num_choosers': num_choosers,
-                'sample_rate': sample_rate,
-                'med_pct_probs_gt_pop_mean': med_pct_probs_gt_pop_mean,
-                'med_max_corr_prob': med_max_corr_prob}])))
+        # save intermediate results
+        metrics = pd.concat((metrics, num_alt_metrics))
+        metrics.to_csv("../iter_err_metrics.csv", index=False)
--- scripts/large_mnl.py
+++ scripts/large_mnl.py
@@ -3,6 +3,7 @@ from jax import jit, vmap, lax, remat
 import jax.numpy as np
 import jax.random as random
 from functools import partial
+import time
 
 
 def softmax(u, scale=1, corr_factor=1):
@@ -60,32 +61,6 @@ def get_mct(choosers, alts, var_mats=None, chooser_alts=None):
         mct = np.hstack((mct, choosers.repeat(sample_size).reshape(-1, 1)))
 
     return mct
-
-
-def create_data(num_alts, pop_to_alts_ratio, num_vars=5):
-
-    # create choosers
-    n_choosers = int(pop_to_alts_ratio * num_alts)
-    choosers = onp.random.lognormal(0, 0.5, n_choosers)  # 1 chooser attribute
-
-    # initialize alts
-    alts = onp.zeros((num_alts, num_vars))
-
-    # dist to CBD
-    alts[:, 0] = onp.random.lognormal(0, 1, num_alts)  # Dist to CBD
-
-    # size terms
-    for i in range(1, num_vars - 1):
-        split_val = int(onp.floor(num_alts * .5))
-        alts[:split_val, i] = onp.random.normal(
-            1, 1, split_val)  # first 1/2 of alts have mean = 1
-        alts[split_val:, i] = onp.random.normal(
-            0.5, 1, num_alts - split_val)  # rest of alts have mean 0.5
-
-    # interaction term
-    alts[:, -1] = onp.random.lognormal(1, .5, num_alts)
-
-    return np.array(choosers), np.array(alts)
 
 
 class large_mnl():
@@ -164,52 +139,106 @@ class large_mnl():
         return choices  # Assuming alternative name starts at 0
 
 
+@jit
+def swop_exp(key, alts_idxs):
+
+    num_alts = alts_idxs.shape[0]
+    w = 1 / num_alts
+    E = -np.log(
+        random.uniform(key, shape=(num_alts,), minval=0, maxval=1))
+    E /= w
+    shuffled = np.argsort(E)
+
+    return shuffled
+
+
 @partial(jit, static_argnums=[3])
 def interaction_sample(
         alts, coeffs, pop_mean_prob, sample_size, chooser_val_key):
 
     chooser_val, key = chooser_val_key
-    total_alts = alts.shape[0]
-
-    # w = 1 / total_alts
-    # E = -np.log(random.uniform(key, shape=(total_alts,), minval=0, maxval=1))
-    # E /= w
-    # shuffled = np.argsort(E)
-
-    shuffled = random.permutation(key, np.arange(total_alts))
-    samp_alts_idxs = shuffled[:sample_size]
-
-    alts2 = alts[samp_alts_idxs, :]
 
     # perform interaction
     idx_alt_intx = -1
-    interacted = alts2[:, idx_alt_intx] / chooser_val
-    alts3 = alts2.at[:, idx_alt_intx].set(interacted)
+    interacted = alts[:, idx_alt_intx] / chooser_val
+    alts2 = alts.at[:, idx_alt_intx].set(interacted)
 
     # compute probs
-    logits = np.dot(alts3, coeffs.T)
-    probas = softmax(logits)
-    probas = probas.flatten()
+    true_logits = np.dot(alts2, coeffs.T)
+    true_probs = softmax(true_logits)
+    true_probs = true_probs.flatten()
+
+    # sample
+    total_alts = alts.shape[0]
+    alts_idxs = np.arange(total_alts)
+    shuffled = swop_exp(key, alts_idxs)
+    samp_alts_idxs = shuffled[:sample_size]
+    alts3 = alts2[samp_alts_idxs, :]
+
+    # compute sample probs
+    samp_logits = np.dot(alts3, coeffs.T)
+    samp_probs = softmax(samp_logits)
+    samp_probs = samp_probs.flatten()
 
     # compute metrics
-    pct_probs_gt_pop_mean = np.sum(probas > pop_mean_prob) / sample_size
-    max_prob = np.nanmax(probas)
 
-    del logits
-    del probas
+    # pct alts with probs greater than true mean prob
+    pct_probs_gt_pop_mean = np.sum(samp_probs > pop_mean_prob) / sample_size
+
+    # true max vs true prob sampled max
+    max_idx = np.nanargmax(samp_probs)
+    true_idx = samp_alts_idxs[max_idx]
+
+    # corrected max prob vs true max
+    max_prob_corr = samp_probs[max_idx] * (sample_size / total_alts)
+
+    del true_logits
+    del true_probs
+    del samp_logits
+    del samp_probs
     del alts2
     del alts3
     del interacted
     del shuffled
-    # del E
+    del alts_idxs
     del samp_alts_idxs
 
-    return max_prob, pct_probs_gt_pop_mean
+    return max_prob_corr, pct_probs_gt_pop_mean, true_idx
+
+
+@jit
+def interaction(alts, coeffs, pop_mean_prob, chooser_val_key):
+    """ Compute logit probs with NO SAMPLING of alts
+    """
+    chooser_val, key = chooser_val_key
+    # total_alts = alts.shape[0]
+
+    # perform interaction
+    idx_alt_intx = -1
+    interacted = alts[:, idx_alt_intx] / chooser_val
+    alts2 = alts.at[:, idx_alt_intx].set(interacted)
+
+    # compute probs
+    logits = np.dot(alts2, coeffs.T)
+    probas = softmax(logits)
+    probas = probas.flatten()
+
+    # # compute metrics
+    # pct_probs_gt_pop_mean = np.sum(probas > pop_mean_prob) / total_alts
+    # max_prob = np.nanmax(probas)
+
+    # del probas
+    del logits
+    del alts2
+    del interacted
+
+    return probas
 
 
 @jit
 @remat
-def interaction(alts, coeffs, pop_mean_prob, chooser_val_key):
+def interaction_iters(
+        alts, coeffs, pop_mean_prob, chooser_val_key, n_splits=10):
 
     chooser_val, key = chooser_val_key
     total_alts = alts.shape[0]
@@ -223,17 +252,68 @@ def interaction(alts, coeffs, pop_mean_prob, chooser_val_key):
     logits = np.dot(alts2, coeffs.T)
     probas = softmax(logits)
     probas = probas.flatten()
+    true_max = np.nanmax(probas)
+    pct_true_probs_gt_pop_mean = np.sum(
+        probas > pop_mean_prob) / total_alts
 
-    # compute metrics
-    pct_probs_gt_pop_mean = np.sum(probas > pop_mean_prob) / total_alts
-    max_prob = np.nanmax(probas)
+    # sample splits
+    alts_idxs = np.arange(total_alts)
+    shuffled = swop_exp(key, alts_idxs)
+    samples = np.array_split(shuffled, n_splits)
 
-    del probas
+    # sample probs
+    samp_alts_idxs = np.asarray([], dtype=int)
+    result_arr = np.zeros((9, 14))
+    for i, sample in enumerate(samples):
+
+        if i == n_splits - 1:
+            continue
+
+        sample_rate = (i + 1) / n_splits
+        sample_size = total_alts * sample_rate
+        samp_alts_idxs = np.concatenate((samp_alts_idxs, sample))
+        alts3 = alts2[samp_alts_idxs]
+        samp_logits = np.dot(alts3, coeffs.T)
+        samp_probs = softmax(samp_logits)
+        samp_probs = samp_probs.flatten()
+        samp_max_idx = np.nanargmax(samp_probs)
+        samp_corr_max = samp_probs[samp_max_idx] * sample_rate
+        true_prob_samp_max = probas[samp_alts_idxs[samp_max_idx]]
+        pct_samp_probs_gt_pop_mean = np.sum(
+            samp_probs > pop_mean_prob) / sample_size
+
+        tmsm_err = true_max - true_prob_samp_max
+        tmsm_pct_err = tmsm_err / true_max
+        tmsm_sq_err = tmsm_err * tmsm_err
+        tmsmc_err = true_max - samp_corr_max
+        tmsmc_pct_err = tmsmc_err / true_max
+        tmsmc_sq_err = tmsmc_err * tmsmc_err
+        cf_err = true_prob_samp_max - samp_corr_max
+        cf_pct_err = cf_err / true_prob_samp_max
+        cf_sq_err = cf_err * cf_err
+        ppgm_err = pct_true_probs_gt_pop_mean - pct_samp_probs_gt_pop_mean
+        ppgm_pct_err = ppgm_err / pct_true_probs_gt_pop_mean
+        ppgm_sq_err = ppgm_err * ppgm_err
+
+        result_arr = result_arr.at[i, :].set(np.array([
+            total_alts, sample_rate,
+            tmsm_err, tmsm_pct_err, tmsm_sq_err,
+            tmsmc_err, tmsmc_pct_err, tmsmc_sq_err,
+            cf_err, cf_pct_err, cf_sq_err,
+            ppgm_err, ppgm_pct_err, ppgm_sq_err]))
+
+    # del probas
     del logits
+    del probas
     del alts2
+    del samp_logits
+    del samp_probs
+    del alts3
     del interacted
+    del shuffled
+    del samples
 
-    return max_prob, pct_probs_gt_pop_mean
+    return result_arr
 
 
 def get_probs(
@@ -255,7 +335,7 @@ def get_probs(
             n_chooser_batches += 1
             if num_choosers % n_chooser_batches != 0:
                 continue
-            elif (mct_size / n_chooser_batches) < max_mct_size:
+            elif (mct_size / n_chooser_batches) < max_mct_size / 3:
                 break
         choosers_per_batch = int(num_choosers / n_chooser_batches)
         print(
@@ -273,25 +353,81 @@ def get_probs(
                 coeffs,
                 pop_mean_prob,
                 sample_size)
+            results = lax.map(
+                vmap(partial_interaction), (choosers, keys))
+            results = [
+                result.reshape((num_choosers, )) for result in results]
         else:
             partial_interaction = partial(
                 interaction,
                 alts,
                 coeffs,
                 pop_mean_prob)
-
-        results = lax.map(vmap(partial_interaction), (choosers, keys))
-        results = [
-            result.reshape((num_choosers, )) for result in results]
+            results = lax.map(
+                vmap(partial_interaction), (choosers, keys))
+            results = results.reshape((num_choosers, num_alts))
 
     else:
         if sample_size < num_alts:
             results = vmap(
                 interaction_sample, in_axes=(None, None, None, None, 0))(
-                alts, coeffs, pop_mean_prob, sample_size, (choosers, keys))
+                alts, coeffs, pop_mean_prob, sample_size,
+                (choosers, keys))
         else:
             results = vmap(
                 interaction, in_axes=(None, None, None, 0))(
                 alts, coeffs, pop_mean_prob, (choosers, keys))
+
+    return results
+
+
+def get_iter_probs(
+        choosers, alts, key, sample_size,
+        batched=False, max_mct_size=1200000000):
+    """VMAP the interaction function over all choosers' values"""
+    now = time.time()
+    num_choosers = choosers.shape[0]
+    key_dim = key.shape[0]
+    keys = random.split(key, num_choosers)
+    num_alts = alts.shape[0]
+    coeffs = np.array([-1, 1, 1, 1, 1])  # dist-to-cbd, sizes 1-3, intx term
+    pop_mean_prob = 1 / num_alts
+    mct_size = num_choosers * num_alts
+    if (batched) & (mct_size > max_mct_size):
+
+        n_chooser_batches = 1
+        while True:
+            n_chooser_batches += 1
+            if num_choosers % n_chooser_batches != 0:
+                continue
+            elif (mct_size / n_chooser_batches) < max_mct_size / 3:
+                break
+        choosers_per_batch = int(num_choosers / n_chooser_batches)
+        print(
+            "Computing probabilities in {0} batches of {1} choosers".format(
+                n_chooser_batches, choosers_per_batch))
+        choosers = choosers.reshape(
+            (n_chooser_batches, choosers_per_batch))
+        keys = keys.reshape(
+            (n_chooser_batches, choosers_per_batch, key_dim))
+
+        partial_interaction = partial(
+            interaction_iters,
+            alts,
+            coeffs,
+            pop_mean_prob)
+        results = lax.map(
+            vmap(partial_interaction), (choosers, keys))
+        results = results.reshape((num_choosers, 9, 14))
+
+    else:
+
+        results = vmap(interaction_iters, in_axes=(None, None, None, 0))(
+            alts, coeffs, pop_mean_prob, (choosers, keys))
+
+    later = time.time()
+    print(
+        "Took {0} s to compute prob. metrics for all sample "
+        "rates by chooser.".format(np.round(later - now)))
 
     return results
